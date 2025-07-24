f5b6bb17a576f6af7b549ae47990abb842501b94
viyk4evr <viykstis@gmail.com>
2025-07-24 16:58:41 +0530

    deployement/new_deployemnt_24_07

---
+++ dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/ttl/accessmentor/model/models"],function(e,t,i){"use strict";return e.extend("com.ttl.accessmentor.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
+//# sourceMappingURL=Component.js.map
---
+++ dist/test/integration/AllJourneys.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/test/Opa5","./arrangements/Startup","./NavigationJourney"],function(e,t){"use strict";e.extendConfig({arrangements:new t,viewNamespace:"com.ttl.accessmentor.view.",autoWait:true})});
+//# sourceMappingURL=AllJourneys.js.map
